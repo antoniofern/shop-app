5e4485dd116914083fbd5e362642ac5ce31cecba
antoniofern <antoniofern.n@gmail.com>
2020-11-17 00:05:12 -0300

    :sparkles: add favorites grid

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,14 +2,46 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FiltersOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FiltersOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FiltersOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Only Favorites'),
+                  value: FiltersOptions.Favorites),
+              PopupMenuItem(child: Text('Show all'), value: FiltersOptions.All),
+            ],
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
